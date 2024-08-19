e03e607e73ff53c8654b0dffbc461a1152588aff
Hardi-Kun <m.hardiansyah0406@gmail.com>
2024-08-19 21:19:24 +0700

    membuat form login

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express   = require('express')
 const app       = express()
 const port      = 3000
+
 const c_beranda = require('./controller/c_beranda')
+const c_auth    = require('./controller/c_auth')
 
 
 app.use( express.urlencoded({extended:false}) )
@@ -12,7 +14,7 @@ app.set('views', './view')
 
 
 app.get('/', c_beranda.index)
-
+app.get('/login', c_auth.form_login)
 
 app.listen(port, ()=>{
     console.log(`Aplikasi sudah siap, buka http://localhost:${port}`)
